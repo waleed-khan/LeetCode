aa1e675fa5d61977bdf64c4fb387ae2661177099
Waleed Khan <wkhan22@uic.edu>
2026-02-21 10:24:51 -0600

    ContainsDuplicate.ts

--- ContainsDuplicate.ts
+++ ContainsDuplicate.ts
@@ -1,7 +1,43 @@
 // LEETCODE 217: CONTAINS DUPLICATES
 
-// SOLUTION 1: SORT AND SEARCH SEQUENTIAL
+/* SOLUTION 1: SORT AND SEARCH SEQUENTIAL */
 
-// SOLUTION 2: CREATE SET, 
+function containsDuplicateSORT(nums: number[]): boolean {
+    const sorted = nums.sort();
+    for (let i = 0; i < sorted.length - 1; i++) {
+        if (sorted[i] === sorted[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+}
 
-// SOLUTION 3: CHECK LENGTH OF SET IS SAME AS LENGTH OF INPUT ARRAY
+// TIME COMPLEXITY: O(n log n) - DUE TO THE SORTING STEP
+// SPACE COMPLEXITY: O(1) - SORTING IN PLACE, NO ADDITIONAL DATA STRUCTURES USED
+
+
+/* SOLUTION 2: CREATE SET AND CHECK IF ELEMENTS ARE IN SET */
+
+function containsDuplicateSET(nums: number[]): boolean {
+    const contains = new Set<number>();
+    for (const num of nums) {
+        if (contains.has(num))
+            return true;
+        contains.add(num);
+    }
+    return false;
+};
+
+// TIME COMPLEXITY: O(n) - WE TRAVERSE THE INPUT ARRAY ONCE
+// SPACE COMPLEXITY: O(n) - IN THE WORST CASE, ALL ELEMENTS IN THE INPUT ARRAY ARE UNIQUE, AND WE STORE THEM IN THE SET
+
+
+/* SOLUTION 3: CHECK LENGTH OF SET IS SAME AS LENGTH OF INPUT ARRAY */
+
+function containsDuplicateLENGTH(nums: number[]): boolean {
+    const unique = new Set(nums);
+    return unique.size !== nums.length;
+}
+
+// TIME COMPLEXITY: O(n) - WE TRAVERSE THE INPUT ARRAY ONCE TO CREATE THE SET
+// SPACE COMPLEXITY: O(n) - IN THE WORST CASE, ALL ELEMENTS IN THE INPUT ARRAY ARE UNIQUE, AND WE STORE THEM IN THE SET
